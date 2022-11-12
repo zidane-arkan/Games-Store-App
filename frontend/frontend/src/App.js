77c3bb65e7fbf80d0c6a0758d8f31737727bbda3
zidane-arkan <zidark897@Gmail.com>
2022-11-12 17:48:22 +0700

    Add React to Frontend

---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
